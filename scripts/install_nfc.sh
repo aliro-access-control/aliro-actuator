32dd8feb61e68d5b1486860507664edf1f568b5d
aschreiner8020 <134614630+aschreiner8020@users.noreply.github.com>
2023-12-19 17:22:49 +0100

    Update install_nfc.sh

--- scripts/install_nfc.sh
+++ scripts/install_nfc.sh
@@ -13,8 +13,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-ACTUATUR_PATH=$(realpath $(dirname "$0")/..)
-cd $ACTUATUR_PATH
+ACTUATOR_PATH=$(realpath $(dirname "$0")/..)
+cd $ACTUATOR_PATH
 mkdir -p third_party
 
 cd third_party
