0d307dad2e6b27aff7da32833b1a425c757008c5
Mike Jongen <mike.jongen@nxp.com>
2024-02-08 10:02:33 +0000

    removed unused imports, added missing fileheader

--- src/aliro_actuator/trust_framework/reader_identifier.py
+++ src/aliro_actuator/trust_framework/reader_identifier.py
@@ -1,3 +1,17 @@
+# Copyright 2023 NXP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from aliro_actuator import READER_GROUP_ID_LENGTH, READER_ID_LENGTH
 from aliro_actuator.trust_framework.errors import InvalidIdentifierError
 
