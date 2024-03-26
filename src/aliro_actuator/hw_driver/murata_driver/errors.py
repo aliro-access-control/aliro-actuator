cd29f36e889516b25eb2506575726e18b06e4fb7
Mike Jongen <mike.jongen@nxp.com>
2024-03-26 15:55:19 +0000

    fcsi of murata driver

---
+++ src/aliro_actuator/hw_driver/murata_driver/errors.py
@@ -0,0 +1,54 @@
+from binascii import hexlify
+
+
+class MurataError(Exception):
+    """
+    Parent class of all errors related to the murata driver.
+    """
+
+    pass
+
+
+class NoResponseError(MurataError):
+    """
+    Raised when no response is received (but one is expected).
+    """
+
+    pass
+
+
+class UnexpectedResponseError(MurataError):
+    """
+    Raised when a response is received, but the response is different from the expected
+    response.
+    """
+
+    pass
+
+
+class ErrorReturnedError(MurataError):
+    """
+    Raised when a response is received, but the response contains an error.
+    """
+
+    pass
+
+
+class FSCIError(MurataError):
+    """
+    Raised when an error in the FSCI protocol is found.
+    """
+
+    pass
+
+
+class InvalidChecksumError(FSCIError):
+    """
+    Raised when a message contains an invalid checksum.
+    """
+
+    def __init__(self, expected: bytes, actual: bytes) -> None:
+        message = "expected: {!r}, actual: {!r}".format(
+            hexlify(expected), hexlify(actual)
+        )
+        super().__init__(message)
