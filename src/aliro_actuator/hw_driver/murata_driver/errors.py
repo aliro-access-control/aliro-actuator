78ca97dabb7e4dde146d31049b39dbe6f6d9d830
Mike Jongen <mike.jongen@nxp.com>
2024-04-02 12:32:32 +0000

    more logging

--- src/aliro_actuator/hw_driver/murata_driver/errors.py
+++ src/aliro_actuator/hw_driver/murata_driver/errors.py
@@ -14,7 +14,8 @@ class NoResponseError(MurataError):
     Raised when no response is received (but one is expected).
     """
 
-    pass
+    def __init__(self) -> None:
+        super().__init__("No response received")
 
 
 class UnexpectedResponseError(MurataError):
@@ -31,20 +32,28 @@ class ErrorReturnedError(MurataError):
     Raised when a response is received, but the response contains an error.
     """
 
-    pass
-
-
-class FSCIError(MurataError):
-    """
-    Raised when an error in the FSCI protocol is found.
-    """
-
-    pass
+    def __init__(self, expected: list[bytes], actual: bytes) -> None:
+        expected_str = ""
+        for expected_bytes in expected:
+            expected_str += "{!r}, ".format(hexlify(expected_bytes))
+        message = "Error returned: expected: {!r}actual: {!r}".format(
+            expected_str, hexlify(actual)
+        )
+        super().__init__(message)
 
 
 class STXError(MurataError):
     """
     Raised when an message with an invalid STX is received.
+    """
+
+    def __init__(self) -> None:
+        super().__init__("Invalid STX received")
+
+
+class FSCIError(MurataError):
+    """
+    Raised when an error in the FSCI protocol is found.
     """
 
     pass
