79624afcc6e9a1707adf9cd90b7d5c7d4f66eeff
Mike Jongen <mike.jongen@nxp.com>
2024-04-09 10:37:12 +0000

    ble message format

---
+++ src/aliro_actuator/transport_protocol/ble_message_format.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+
+from enum import IntEnum
+
+
+class ProtocolType(IntEnum):
+    AP = 0x00
+    UWB_RANGING_SERVICE = 0x01
+    NOTIFICATION = 0x02
+    SUPPLEMENTARY_SERVICE = 0x03
+    THIRD_PARTY_APP = 0x04
+
+
+class AP_ID(IntEnum):
+    AP_RQ = 0x00
+    AP_RS = 0x01
+
+
+class Notification_ID(IntEnum):
+    EVENT = 0x00
+    RANGING = 0x01
+    READER_STATUS_CHANGED = 0x02
+    READER_STATUS_ACCESS_PROTOCOL_COMPLETED = 0x03
+    RKE_REQUEST = 0x04
+    INITIATE_ACCESS_PROTOCOL = 0x05
+    INITIATE_ACCESS_PROTOCOL_RKE = 0x06
+
+
+class BleMessage:
+    def __init__(self, header: int, id: int, payload: bytes) -> None:
+        self.header = header
+        self.id = id
+        self.payload = payload
+
+    @classmethod
+    def from_bytes(cls, input: bytes) -> BleMessage:
+        header = input[0]
+        id = input[1]
+        length = int.from_bytes(input[2:4], "big")
+        payload = input[4 : 4 + length]
+        return BleMessage(header, id, payload)
+
+    def to_bytes(self) -> None:
+        output = bytearray()
+        output.append(self.header)
+        output.append(self.id)
+        output.extend(len(self.payload).to_bytes(2, "big"))
+        output.extend(self.payload)
+
+
+class Event_AttributeID(IntEnum):
+    BUSY = 0x00
+    GENERAL_ERROR = 0x01
+
+
+class GeneralError_Values(IntEnum):
+    UNKNOWN_ERROR = 0x00
+    RESOURCE_UNAVAILABLE = 0x01
+    WRONG_PARAMETERS = 0x02
+    URSK_UNAVAILABLE = 0x3
+
+
+class BleAttribute:
+    def __init__(self, id: int, value: bytes) -> None:
+        self.id = id
+        self.value = value
+
+    @classmethod
+    def from_bytes(cls, input: bytes) -> BleAttribute:
+        id = input[0]
+        length = input[1]
+        value = input[1 : 1 + length]
+        return BleAttribute(id, value)
+
+    def to_bytes(self) -> None:
+        output = bytearray()
+        output.append(self.id)
+        output.append(len(self.value))
+        output.extend(self.value)
