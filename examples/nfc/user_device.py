7935bf12baac4d1acdeaeb57df10db33aeb1de5a
Mike Jongen <mike.jongen@nxp.com>
2024-04-17 14:42:58 +0000

    user device log kpersistent

--- examples/nfc/user_device.py
+++ examples/nfc/user_device.py
@@ -19,10 +19,14 @@ PROJECT_PATH = os.path.join(os.getcwd(), "src/")
 sys.path.append(PROJECT_PATH)
 
 from aliro_actuator.access_protocol.defines import TransportProtocol
-from aliro_actuator.access_protocol.user_device import UserDevice
+from aliro_actuator.access_protocol.user_device import UserDevice, UserStorage
 from aliro_actuator.trust_framework.access_credential import AccessCredential
 from aliro_actuator.trust_framework.key import KeyPair, PublicKey
-from examples.nfc.common import READER_GROUP_IDENTIFIER, READER_SUB_GROUP_IDENTIFIER
+from examples.nfc.common import (
+    KPERSISTENT,
+    READER_GROUP_IDENTIFIER,
+    READER_SUB_GROUP_IDENTIFIER,
+)
 
 if __name__ == "__main__":
     reader_public_key_pem = open("examples/nfc/reader_public_key.pem", "rt")
@@ -35,10 +39,14 @@ if __name__ == "__main__":
     credential_keypair = KeyPair(private_key_pem.read(), public_key_pem.read())
     access_credentials = [AccessCredential(credential_keypair, reader_identifier_list)]
 
+    userstorage = UserStorage()
+    userstorage.add_kpersistent(KPERSISTENT, READER_SUB_GROUP_IDENTIFIER)
+
     reader = UserDevice(
         transport_protocol=TransportProtocol.NFC,
         access_credentials=access_credentials,
         mailbox=0x20,
+        user_device_storage=userstorage,
     )
     reader.transaction_initiation()
     reader.main_loop()
