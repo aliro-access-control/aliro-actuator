eeab1bd4a4ca206ef5fb26c06057f6c17aa8bd1f
Mike Jongen <mike.jongen@nxp.com>
2024-05-22 11:03:59 +0000

    added Reader Status Access Protocol Completed Message

--- src/aliro_actuator/transport_protocol/ble_message_format.py
+++ src/aliro_actuator/transport_protocol/ble_message_format.py
@@ -2,7 +2,9 @@ from __future__ import annotations
 
 from enum import IntEnum
 
+from aliro_actuator import Global
 from aliro_actuator.hw_driver.murata_driver.endianness import change_endianness
+from aliro_actuator.transport_protocol.errors import BLEMessageError
 
 
 class ProtocolType(IntEnum):
@@ -57,11 +59,27 @@ class Event_AttributeID(IntEnum):
     GENERAL_ERROR = 0x01
 
 
+class AccessProtocolCompleted_AttributeID(IntEnum):
+    READER_INFORMATION_ATTRIBUTE_ID = 0x00
+
+
 class GeneralError_Values(IntEnum):
     UNKNOWN_ERROR = 0x00
     RESOURCE_UNAVAILABLE = 0x01
     WRONG_PARAMETERS = 0x02
     URSK_UNAVAILABLE = 0x3
+
+
+class UnsolicitedReaderStatusReporting_Values(IntEnum):
+    DO_NOT_SEND = 0
+    SEND_TO_EACH_CONNECTED = 1
+    SEND_ONLY_TO_THIS = 2
+
+
+class ReaderStatusInformation_Values(IntEnum):
+    SECURED = 0
+    UNSECURED = 1
+    JAMMED = 2
 
 
 class BleAttribute:
@@ -82,3 +100,54 @@ class BleAttribute:
         output.append(len(self.value))
         output.extend(self.value)
         return bytes(output)
+
+    def parse_as_access_protocol_completed_attribute(self) -> None:
+        Global.logger.info("Parsing Reader Status Access Protocol Completed attribute")
+        if (
+            self.id
+            != AccessProtocolCompleted_AttributeID.READER_INFORMATION_ATTRIBUTE_ID
+        ):
+            raise BLEMessageError(
+                "Invalid attribute in ble message: 0x{:02x}".format(self.id)
+            )
+
+        Global.logger.info("Parsing attribute: Reader information")
+        unsolicited_reader_status_reporting_int = self.value[0] >> 5
+        reader_status_information_int = self.value[1]
+        try:
+            self.unsolicited_reader_status_reporting = (
+                UnsolicitedReaderStatusReporting_Values(
+                    unsolicited_reader_status_reporting_int
+                )
+            )
+        except ValueError as error:
+            raise BLEMessageError(
+                "unsolicited reader status reporting has invalid value: 0x{:02x}".format(
+                    unsolicited_reader_status_reporting_int
+                )
+            ) from error
+        Global.logger.debug(
+            "unsolicited reader status reporting has a valid value: "
+            "0x{!r} ({!r})".format(
+                self.unsolicited_reader_status_reporting,
+                self.unsolicited_reader_status_reporting.name,
+            )
+        )
+
+        try:
+            self.reader_status_information = ReaderStatusInformation_Values(
+                reader_status_information_int
+            )
+        except ValueError as error:
+            raise BLEMessageError(
+                "reader status information has invalid value: 0x{:02x}".format(
+                    reader_status_information_int
+                )
+            ) from error
+        Global.logger.debug(
+            "reader status information has a valid value: "
+            "0x{!r} ({!r})".format(
+                self.reader_status_information,
+                self.reader_status_information.name,
+            )
+        )
