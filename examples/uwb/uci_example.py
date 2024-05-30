08ef22bb359c8182a7c39437ef71ab0669a51736
Mircea Caprioru <mircea.capriuoru@nxp.com>
2024-05-30 11:25:20 +0000

    update uwb driver example
    Signed-off-by: Mircea Caprioru <mircea.capriuoru@nxp.com>

--- examples/uwb/uci_example.py
+++ examples/uwb/uci_example.py
@@ -11,8 +11,11 @@ def main():
         dev_type=uci.APP_CFG.DEVICE_TYPE.CONTROLEE,
     )
 
-    print(f"Pulse Shape Combo: {driver.get_pulse_shape_combination()}")
-    print(f"Channel Bitmask: {driver.get_channel_bitmask()}")
+    print(f"pulseshape_combo: {hex(driver.get_pulse_shape_combination())}")
+    print(f"channel_bitmask: {hex(driver.get_channel_bitmask())}")
+    print(f"slot bitmask: {hex(driver.get_slot_bitmask())}")
+    print(f"sync_code_index_bitmask: {hex(driver.get_sync_code_bitmask())}")
+    print(f"hopping_config_bitmask = {hex(driver.get_hopping_config_bitmask())}")
 
 
 if __name__ == "__main__":
