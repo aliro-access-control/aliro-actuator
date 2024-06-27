6220bff269e9ff5832a0c2e8ad89fc9671801d84
Mircea Caprioru <mircea.capriuoru@nxp.com>
2024-06-27 08:38:54 +0300

    remove unnecessary logs and fix pulse shape length
    Signed-off-by: Mircea Caprioru <mircea.capriuoru@nxp.com>

--- src/aliro_actuator/transport_protocol/ble_message_format.py
+++ src/aliro_actuator/transport_protocol/ble_message_format.py
@@ -606,7 +606,7 @@ class BleMessage(Message):
         uwb_configuration_id_attr = BleAttribute(
             UWB_AttributeID.UWB_CONFIGURATION_IDENTIFIER, data
         )
-        data = pulse_shape_combination.to_bytes(1, "big")
+        data = pulse_shape_combination.to_bytes(3, "big")
         pulse_shape_combination_attr = BleAttribute(
             UWB_AttributeID.PULSE_SHAPE_COMBO, data
         )
@@ -646,7 +646,7 @@ class BleMessage(Message):
         uwb_configuration_id_attr = BleAttribute(
             UWB_AttributeID.UWB_CONFIGURATION_IDENTIFIER, data
         )
-        data = pulse_shape_combination.to_bytes(1, "big")
+        data = pulse_shape_combination.to_bytes(3, "big")
         pulse_shape_combination_attr = BleAttribute(
             UWB_AttributeID.PULSE_SHAPE_COMBO, data
         )
