163134399ff872f5972729bd3f1ca4586addf056
Mircea Caprioru <mircea.capriuoru@nxp.com>
2024-07-23 16:09:23 +0300

    fix pulseshape combo
    Signed-off-by: Mircea Caprioru <mircea.capriuoru@nxp.com>

--- src/aliro_actuator/transport_protocol/ble_message_format.py
+++ src/aliro_actuator/transport_protocol/ble_message_format.py
@@ -697,7 +697,7 @@ class BleMessage(Message):
         uwb_configuration_id_attr = BleAttribute(
             UWB_AttributeID.UWB_CONFIGURATION_IDENTIFIER, data
         )
-        data = pulse_shape_combination.to_bytes(1, "big")
+        data = pulse_shape_combination.to_bytes(3, "big")
         pulse_shape_combination_attr = BleAttribute(
             UWB_AttributeID.PULSE_SHAPE_COMBO, data
         )
@@ -739,7 +739,7 @@ class BleMessage(Message):
         uwb_configuration_id_attr = BleAttribute(
             UWB_AttributeID.UWB_CONFIGURATION_IDENTIFIER, data
         )
-        data = pulse_shape_combination.to_bytes(1, "big")
+        data = pulse_shape_combination.to_bytes(3, "big")
         pulse_shape_combination_attr = BleAttribute(
             UWB_AttributeID.PULSE_SHAPE_COMBO, data
         )
