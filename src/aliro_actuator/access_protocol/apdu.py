b57a1cd6e6ca07b305f3eca7e741c151dca5cccf
wimnxp <wim.nuyts@nxp.com>
2024-12-06 16:23:29 +0100

    Using TLVIndex

--- src/aliro_actuator/access_protocol/apdu.py
+++ src/aliro_actuator/access_protocol/apdu.py
@@ -718,7 +718,7 @@ class Command(APDUMessage):
             )
 
             Global.logger.debug("Data needs to be verified during handling")
-            TLV.verifySequence(self.decrypted_payload, TLV_EXCHANGE_CMD)
+            TLV.verifySequence(self.decrypted_payload, TLVIndex.TLV_EXCHANGE_CMD)
 
             self.payload_tlv = TLV.from_bytes(self.decrypted_payload, recursive=False)
             Global.logger.debug(
