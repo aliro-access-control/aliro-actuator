73dca3d20303718019daacd12822dde88f4a0f99
Mircea Caprioru <mircea.capriuoru@nxp.com>
2025-04-09 16:49:26 +0300

    fix command status

--- src/aliro_actuator/access_protocol/user_device.py
+++ src/aliro_actuator/access_protocol/user_device.py
@@ -12,11 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import ucitool.base_uci.helpers.uci_helper as uci
-
 from binascii import hexlify
 from enum import Enum
 from os import urandom
+
+import ucitool.base_uci.helpers.uci_helper as uci
 
 from aliro_actuator import Global
 from aliro_actuator.access_document.access_credential import AccessDocument
@@ -57,18 +57,24 @@ from aliro_actuator.access_protocol.errors import (
     InvalidAIDError,
     InvalidCLAError,
     InvalidCommandError,
+    InvalidHoppingConfig,
     InvalidINSError,
     InvalidParameterError,
+    InvalidPulseShapeCombo,
+    InvalidSyncCodeIndex,
+    InvalidUWBSessionId,
     SessionError,
     UnexpectedBLEMessageError,
     UnexpectedCommandError,
     VersionError,
-    InvalidPulseShapeCombo,
-    InvalidHoppingConfig,
-    InvalidUWBSessionId,
-    InvalidSyncCodeIndex
 )
 from aliro_actuator.access_protocol.mailbox import Mailbox
+from aliro_actuator.hw_driver.murata_driver.uwb_driver import (
+    Channel,
+    HoppingConfig,
+    UCIHoppingConfig,
+    pulse_shape_combo,
+)
 from aliro_actuator.transport_protocol import Mode, TransportProtocolBase
 from aliro_actuator.transport_protocol.ble_encryption import get_ble_encryption
 from aliro_actuator.transport_protocol.ble_message_format import (
@@ -84,12 +90,6 @@ from aliro_actuator.transport_protocol.errors import (
     InvalidProtocolTypeError,
     NoDeviceConnectedError,
     UnexpectedMessageTypeError,
-)
-from aliro_actuator.hw_driver.murata_driver.uwb_driver import (
-    UCIHoppingConfig,
-    HoppingConfig,
-    pulse_shape_combo,
-    Channel
 )
 from aliro_actuator.trust_framework.access_credential import AccessCredential
 from aliro_actuator.trust_framework.certificate import Certificate
@@ -966,6 +966,11 @@ class UserDevice(Device):
                 "Could not find key for reader identifier in access credential: "
                 "{!r}".format(hexlify(self.session.reader_group_identifier))
             )
+            
+        if hasattr(auth0_command, "tlv_check"):
+            command_status = auth0_command.tlv_check
+        else:
+            command_status = True
 
         if self.session.get_transaction_type() == Transaction.STANDARD:
             Global.logger.info("Standard transaction requested")
@@ -973,7 +978,7 @@ class UserDevice(Device):
 
             await self.response_auth0(
                 self.session.get_credential_epubkey().as_bytes(),
-                command_status=auth0_command.tlv_check
+                command_status=command_status
             )
         elif self.session.get_transaction_type() == Transaction.FAST:
             Global.logger.info("Fast transaction requested")
@@ -1022,7 +1027,7 @@ class UserDevice(Device):
             await self.response_auth0(
                 credential_epubk=self.session.get_credential_epubkey().as_bytes(),
                 cryptogram=cryptogram,
-                command_status=auth0_command.tlv_check
+                command_status=command_status
             )
 
         Global.logger.info("Handling AUTH0 command done")
