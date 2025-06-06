26fa80c7fba52ff591caaf447267955493e173d5
Purnank H. G. <purnank@google.com>
2025-06-06 14:18:55 +0800

    Fix examples/uwb/uci_example.py
    The example will now connect to MurataBoard
    and print print capabilities.
    
    The session_id is removed
    and baud rate is updated as used by the board.

--- examples/uwb/uci_example.py
+++ examples/uwb/uci_example.py
@@ -6,18 +6,21 @@ from aliro_actuator.hw_driver.murata_driver.uwb_driver import MurataUWBDriver
 
 
 async def main():
-    driver = MurataUWBDriver("/dev/ttyUSB0", 576000)
+    driver = MurataUWBDriver("/dev/ttyUSB0", 230400)
     await driver.uci_initialize(
-        session_id=0,
         dev_role=uci.APP_CFG.DEVICE_ROLE.RESPONDER,
         dev_type=uci.APP_CFG.DEVICE_TYPE.CONTROLEE,
     )
 
-    print(f"pulseshape_combo: {hex(driver.get_pulse_shape_combination())}")
-    print(f"channel_bitmask: {hex(driver.get_channel_bitmask())}")
-    print(f"slot bitmask: {hex(driver.get_slot_bitmask())}")
-    print(f"sync_code_index_bitmask: {hex(driver.get_sync_code_bitmask())}")
-    print(f"hopping_config_bitmask = {hex(driver.get_hopping_config_bitmask())}")
+    await driver.get_capabilities()
+
+    print(f"slot_bitmask: {hex(driver.slot_bitmask)}")
+    print(f"sync_code_index_bitmask: {hex(driver.sync_code_index_bitmask)}")
+    print(f"hopping_config_bitmask: {hex(driver.hopping_config_bitmask)}")
+    print(f"channel_bitmask: {hex(driver.channel_bitmask)}")
+    print(f"protocol_versions: {hex(driver.protocol_versions)}")
+    print(f"uwb_config_id_support: {hex(driver.uwb_config_id_support)}")
+    print(f"pulseshape_combo_support: {hex(driver.pulseshape_combo_support)}")
 
 
 if __name__ == "__main__":
